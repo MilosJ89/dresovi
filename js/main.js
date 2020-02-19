b08f89eb701f7bd135751c8a20f1d2d40d57ea71
Milos <jovanovicmilos.kg@gmail.com>
2020-02-19 22:53:42 +0100

    function for colors

--- js/main.js
+++ js/main.js
@@ -51,7 +51,6 @@ function createContent () {
 
 createContent();
 
-
 /** Create shirt model at left page */
 function createShirt () {
     document.getElementById('modelMajica').innerHTML = majica1;
@@ -60,63 +59,6 @@ function createShirt () {
 }
 
 createShirt();
-
-
-/** Create majice */
-function createMajice() {
-    document.getElementById('models').innerHTML = '';
-    for(let majica of majice) {
-        document.getElementById('models').innerHTML += majica;
-    }
-/** TODO */
-    document.getElementById('majica1').addEventListener('click', ()=> {
-        document.getElementById('modelMajica').innerHTML = majica1;
-    });
-
-    document.getElementById('majica2').addEventListener('click', ()=> {
-        document.getElementById('modelMajica').innerHTML = majica2;
-    });
-
-    document.getElementById('majica3').addEventListener('click', ()=> {
-        document.getElementById('modelMajica').innerHTML = majica3;
-    });
-}
-
-/** Create sorcevi */
-function createSorcevi() {
-    document.getElementById('models').innerHTML = '';
-    for(let sorc of sorcevi) {
-        document.getElementById('models').innerHTML += sorc;
-    }
-
-    document.getElementById('sorc1').addEventListener('click', ()=> {
-        document.getElementById('modelSorc').innerHTML = sorc1;
-    });
-
-    document.getElementById('sorc2').addEventListener('click', ()=> {
-        document.getElementById('modelSorc').innerHTML = sorc2;
-    })
-}
-
-/** Create stucne */
-function createStucne() {
-    document.getElementById('models').innerHTML = '';
-    for(let stucna of stucne) {
-        document.getElementById('models').innerHTML += stucna;
-    }
-
-    document.getElementById('stucne1').addEventListener('click', ()=> {
-        document.getElementById('modelStucne').innerHTML = stucne1;
-    });
-
-    document.getElementById('stucne2').addEventListener('click', ()=> {
-        document.getElementById('modelStucne').innerHTML = stucne2;
-    });
-
-    document.getElementById('stucne3').addEventListener('click', ()=> {
-        document.getElementById('modelStucne').innerHTML = stucne3;
-    });
-}
 
 /**Create colors */
 function createColors () {
@@ -143,18 +85,6 @@ function createColors () {
         
         let textRukav = `<p class='text'>Boja rukava<p/>`;
         document.getElementById('colorRukav').innerHTML += textRukav;
-
-    // let colorStrafta = `<div id='colorStrafta' class='colors'></div>`;
-    // document.getElementById('color').innerHTML += colorStrafta;
-
-    //     let inputColorStrafta = `<input id='inputColorStrafta' class='inputColors' type='color' value='#00ff00'>`;
-    //     document.getElementById('colorStrafta').innerHTML = inputColorStrafta;
-
-    //     let spanColorStrafta = `<span class='arrowDown'></span>`;
-    //     document.getElementById('colorStrafta').innerHTML += spanColorStrafta;
-        
-    //     let textStrafta = `<p class='text'>Boja strafte<p/>`;
-    //     document.getElementById('colorStrafta').innerHTML += textStrafta;
 
     let colorSorc = `<div id='colorSorc' class='colors'></div>`;
     document.getElementById('color').innerHTML += colorSorc;
@@ -183,193 +113,91 @@ function createColors () {
 
 createColors();
 
+/** Create majice */
+function createMajice() {
+    document.getElementById('models').innerHTML = '';
+    for(let majica of majice) {
+        document.getElementById('models').innerHTML += majica;
+    }
+/** TODO */
+    document.getElementById('majica1').addEventListener('click', ()=> {
+        document.getElementById('modelMajica').innerHTML = majica1;
+    });
 
+    document.getElementById('majica2').addEventListener('click', ()=> {
+        document.getElementById('modelMajica').innerHTML = majica2;
+    });
 
-/** Choose model */
-        // document.getElementById('majica2').addEventListener('click', function() {document.getElementById('modelMajica').innerHTML = majica2});
+    document.getElementById('majica3').addEventListener('click', ()=> {
+        document.getElementById('modelMajica').innerHTML = majica3;
+    });
+}
 
-// function chooseMajica() {
-//     document.getElementById('modelMajica').innerHTML = majica2;
-// }
+/** Create sorcevi */
+function createSorcevi() {
+    document.getElementById('models').innerHTML = '';
+    for(let sorc of sorcevi) {
+        document.getElementById('models').innerHTML += sorc;
+    }
+/** TODO */
+    document.getElementById('sorc1').addEventListener('click', ()=> {
+        document.getElementById('modelSorc').innerHTML = sorc1;
+    });
 
-/** Create models shirts at right page */
-// function createModels () {
-//     for (let model of modeli) {
-//         document.getElementById('models').innerHTML += model;    
-//     }
-// }
+    document.getElementById('sorc2').addEventListener('click', ()=> {
+        document.getElementById('modelSorc').innerHTML = sorc2;
+    })
+}
 
-// createModels();
+/** Create stucne */
+function createStucne() {
+    document.getElementById('models').innerHTML = '';
+    for(let stucna of stucne) {
+        document.getElementById('models').innerHTML += stucna;
+    }
+/** TODO */
+    document.getElementById('stucne1').addEventListener('click', ()=> {
+        document.getElementById('modelStucne').innerHTML = stucne1;
+    });
 
-/** Choose model TODO */
-// function chooseModel1() {
-//     document.getElementById('model').innerHTML = model1;
-//     document.getElementById('model1').setAttribute('class', 'modelSvg');
-// };
+    document.getElementById('stucne2').addEventListener('click', ()=> {
+        document.getElementById('modelStucne').innerHTML = stucne2;
+    });
 
-// function chooseModel2() {
-//     document.getElementById('model').innerHTML = model2;
-//     document.getElementById('model2').setAttribute('class', 'modelSvg');
-// };
+    document.getElementById('stucne3').addEventListener('click', ()=> {
+        document.getElementById('modelStucne').innerHTML = stucne3;
+    });
+}
 
-// function chooseModel3() {
-//     document.getElementById('model').innerHTML = model3;
-//     document.getElementById('model3').setAttribute('class', 'modelSvg');
-// };
-
-// function chooseModel4() {
-//     document.getElementById('model').innerHTML = model4;
-//     document.getElementById('model4').setAttribute('class', 'modelSvg');
-// };
-
-// function chooseModel5() {
-//     document.getElementById('model').innerHTML = model5;
-//     document.getElementById('model5').setAttribute('class', 'modelSvg');
-// };
-
-
-/** Choose color dres */
-// document.getElementById('inputColorDres').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorDres').value;
-//             document.getElementById(`majicaModel${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
-
+/** Color majica */
 document.getElementById('inputColorDres').addEventListener('input', () => {
-    let color = document.getElementById('inputColorDres').value; 
-    document.getElementById(`majicaX`).setAttribute('fill', color);
-    
-})
+    for(let i = 1; i < 4; i++) {
+        if(document.getElementById(`majica${i}`).parentNode.id === `modelMajica`) {
+            let color = document.getElementById('inputColorDres').value; 
+            document.getElementById(`majica${i}Napred`).setAttribute('fill', color);
+            document.getElementById(`majica${i}Ledja`).setAttribute('fill', color);
+        }
+    }
+});
 
-// /** Choose color rukav */
-// document.getElementById('inputColorRukav').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorRukav').value;
-//             document.getElementById(`rukav1Model${i}`).setAttribute('fill', color);
-//             document.getElementById(`rukav2Model${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
 
-// /** Choose color strafta */
-// document.getElementById('inputColorStrafta').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorStrafta').value;
-//             document.getElementById(`straftaModel${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
+/** Color sorc */
+document.getElementById('inputColorSorc').addEventListener('input', () => {
+    for(let i = 1; i < 3; i++) {
+        if(document.getElementById(`sorc${i}`).parentNode.id === `modelSorc`) {
+            let color = document.getElementById('inputColorSorc').value; 
+            document.getElementById(`sorc${i}Boja`).setAttribute('fill', color);
+        }
+    }
+});
 
-/** Page dresovi */
-// function createDresovi () {
-//     document.getElementById('models').innerHTML = '';
-    
-//     createShirt();
-//     createModels();
-//     createColors();
-
-//     /** Choose color dres */
-// document.getElementById('inputColorDres').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorDres').value;
-//             document.getElementById(`majicaModel${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
-
-// /** Choose color rukav */
-// document.getElementById('inputColorRukav').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorRukav').value;
-//             document.getElementById(`rukav1Model${i}`).setAttribute('fill', color);
-//             document.getElementById(`rukav2Model${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
-
-// /** Choose color strafta */
-// document.getElementById('inputColorStrafta').addEventListener('input', () => {
-//     for(let i = 1; i < 6; i++) {
-//         if(document.getElementById('model').firstChild.id === `model${i}`) {
-//             let color = document.getElementById('inputColorStrafta').value;
-//             document.getElementById(`straftaModel${i}`).setAttribute('fill', color);
-//         }
-//     }
-// });
-// }
-
-// /** Page trenerke */
-// function createTrenerka () {
-//     document.getElementById('model').innerHTML = '';
-//     document.getElementById('color').innerHTML = '';
-//     document.getElementById('models').innerHTML = '';
-
-//     document.getElementById('model').innerHTML = trenerka;
-//     document.getElementById('model').setAttribute('class', 'modelSvg');
-
-//     document.getElementById('models').innerHTML = trenerka;
-// /** Boja trenerke */
-//     let bojaTrenerke = `<p class='bojaTrenerke'>Boja trenerke</p>`;
-//     document.getElementById('color').innerHTML = bojaTrenerke;
-
-//     let colorTrenerka = `<div id='selectTrenerka'></div>`
-//     document.getElementById('color').innerHTML += colorTrenerka;
-
-//     let kvadrat = `<span id='kvadrat'></span>`;
-//     document.getElementById('selectTrenerka').innerHTML += kvadrat;
-
-//     let colorParagraph = `<p id='colorParagraph'>Crvena</p>`;
-//     document.getElementById('selectTrenerka').innerHTML += colorParagraph;
-
-//     let strelica = `<span class='strelica'></span>`;
-//     document.getElementById('selectTrenerka').innerHTML += strelica;
-
-//         let dropDown = `<div id='dropDown'></div>`;
-//         document.getElementById('selectTrenerka').innerHTML += dropDown;
-
-//             let colorRed = `<div id='colorRed' onclick='colorRed()'>Crvena</div>`;
-//             document.getElementById('dropDown').innerHTML = colorRed;
-
-//                 let kvadratRed = `<span class='kvadratRed'></span>`;
-//                 document.getElementById('colorRed').innerHTML += kvadratRed;
-
-//             let colorYellow = `<div id='colorYellow' onclick='colorYellow()'>Zuta</div>`;
-//             document.getElementById('dropDown').innerHTML += colorYellow;
-
-//                 let kvadratYellow = `<span class='kvadratYellow'></span>`;
-//                 document.getElementById('colorYellow').innerHTML += kvadratYellow;
-
-//             let colorBlue = `<div id='colorBlue' onclick='colorBlue()'>Plava</div>`;
-//             document.getElementById('dropDown').innerHTML += colorBlue;
-
-//                 let kvadratBlue = `<span class='kvadratBlue'></span>`;
-//                 document.getElementById('colorBlue').innerHTML += kvadratBlue;
-// /** Color rukavi */    
-// }
-
-// /** Color trenerka */
-// function colorRed () {
-//     document.getElementById('trenerka').setAttribute('fill', 'red');
-//     document.getElementById('colorParagraph').textContent = 'Crvena';
-//     document.getElementById('kvadrat').style.background = 'red';
-// }
-
-// function colorYellow () {
-//     document.getElementById('trenerka').setAttribute('fill', 'yellow');
-//     document.getElementById('colorParagraph').textContent = 'Zuta';
-//     document.getElementById('kvadrat').style.background = 'yellow';
-// }
-
-// function colorBlue () {
-//     document.getElementById('trenerka').setAttribute('fill', 'blue');
-//     document.getElementById('colorParagraph').textContent = 'Plava';
-//     document.getElementById('kvadrat').style.background = 'blue';
-// }
-
+/** Color stucne */
+document.getElementById('inputColorStucne').addEventListener('input', () => {
+    for(let i = 1; i < 4; i++) {
+        if(document.getElementById(`stucne${i}`).parentNode.id === `modelStucne`) {
+            let color = document.getElementById('inputColorStucne').value; 
+            document.getElementById(`stucna${i}Leva`).setAttribute('fill', color);
+            document.getElementById(`stucna${i}Desna`).setAttribute('fill', color);
+        }
+    }
+});
